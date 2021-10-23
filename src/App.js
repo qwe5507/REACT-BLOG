bede2477a289ce337f6caa8de4c9f82cde08c153
qwe5507 <apvmffkdls@naver.com>
2021-10-23 14:56:57 +0900

    git test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          오랜만이야.
+          오랜만이야. 리액트
         </p>
         <a
           className="App-link"
