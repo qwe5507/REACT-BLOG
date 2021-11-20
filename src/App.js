5278ae6fdb672e7f172d63799121178505fa0bf3
qwe5507 <apvmffkdls@naver.com>
2021-11-20 22:19:57 +0900

    JSX를 이용해 HTML 페이지 제작해보는건 처음이겠죠

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let posts = '강남 고기 맛집'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          오랜만이야. 리액트
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div style={{color: 'blue', fontSize: '30px'}}>개발 Blog</div>
+      </div>
+      {/* <img src={logo}/> */}
+      <h4>{ posts }</h4>
     </div>
   );
 }
