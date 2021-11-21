9203a42ad214e4737d28aed442a984df559cbb87
qwe5507 <apvmffkdls@naver.com>
2021-11-21 21:15:34 +0900

    중요한 데이터는 변수말고 리액트 state로 만들랬죠

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  let posts = '강남 고기 맛집'
+  let [글제목, 글제목변경]= useState(["남자 코트 추천","강남 우동 맛집"]);
+  
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={{color: 'blue', fontSize: '30px'}}>개발 Blog</div>
+        <div style={{color: 'blue', fontSize: '30px'}}>
+          이진강 개발 Blog
+          </div>
       </div>
-      {/* <img src={logo}/> */}
-      <h4>{ posts }</h4>
+      <div className="list">
+        <h3> { 글제목[0] } </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목[1] } </h3>
+        <p>3월 10일 발행</p>
+        <hr/>
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
