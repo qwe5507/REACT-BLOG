c1cf503c1794743fe39faaacd81514b637fceb6a
qwe5507 <apvmffkdls@naver.com>
2021-11-21 21:44:21 +0900

    버튼에 기능개발을 해보자 & 리액트 state변경하는 법- 과제

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  let [글제목, 글제목변경]= useState(["남자 코트 추천","선릉 맛집", "회사가기 싫은 이유"]);
-  
+  let [글제목, 글제목변경] = useState(["남자 코트 추천","선릉 맛집", "회사가기 싫은 이유"]);
+  let [따봉갯수, 따봉갯수변경] = useState(0);
+
+  function 제목변경하는숙제() {
+    let 임시방편 = [...글제목];
+    임시방편[0] = "여자 코트 추천";
+    글제목변경(임시방편);
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
@@ -12,8 +19,9 @@ function App() {
           이진강 개발 Blog
           </div>
       </div>
+      <button onClick={ () => 제목변경하는숙제() }>숙제버튼</button>
       <div className="list">
-        <h3> { 글제목[0] } </h3>
+        <h3> { 글제목[0] } <span onClick={ () => 따봉갯수변경(따봉갯수 + 1) }>👍</span> {따봉갯수} </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
